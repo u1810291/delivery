9e84c44ce9e2a4a69f6b71027b2ff52464e59591
Otabek <o.rizakulov@student.inha.uz>
2020-07-11 23:56:16 +0500

    login completed

--- delivery/settings.py
+++ delivery/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api_pickup',
+    'corsheaders',
     'rest_framework'
 ]
 
@@ -63,6 +64,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'delivery.urls'
@@ -133,4 +135,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+]
+
 STATIC_URL = '/static/'
+
+JWT_AUTH = {
+    'JWT_VERIFY_EXPIRATION': False,
+
+    # allow refreshing of tokens
+    'JWT_ALLOW_REFRESH': True,
+}
